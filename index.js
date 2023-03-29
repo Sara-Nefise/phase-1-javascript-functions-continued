36afc2cb2a2cbdc44ce5d670333fc53a0c940203
saranafisa <sara_nafisa@protonmail.com>
2023-03-29 17:02:52 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`;
+    }
+    
+    
+    function wrapAdjective(special){
+        return function (activity) {
+            return `You are ${special}${activity}${special}!`;
+          };
+        }
+
+console.log(wrapAdjective("%")("a dedicated programmer"));
